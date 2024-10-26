35a9b1231b67f303e2e553d82578540100a05a1d
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-10-26 16:18:24 -0500

    stylesheets + platform specific handling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView, Alert, TouchableWithoutFeedback, TouchableOpacity, TouchableNativeFeedback, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Alert, TouchableWithoutFeedback, TouchableOpacity, TouchableNativeFeedback, Button, Platform } from 'react-native';
 
 export default function App() {
   console.log("App Executed")
@@ -11,8 +11,7 @@ export default function App() {
       <Button 
       color="orange"
       title="Click Me" 
-      onPress={() => Alert.prompt("my title", "my message", text => console.log(text))}
-        />
+      onPress={() => Alert.alert("My title", "My message", [{text: "Yes", onPress: () => console.log("button pressed - Yes")}, {text: "No", onPress: () => console.log("button pressed - No")}] )}/>
       <Text numberOfLines={1} onPress={handlePress}>
         React Native Tutorial
       </Text>
@@ -34,9 +33,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  }
 });
 
-// Alert.alert("My title", "My message", [{text: "Yes", onPress: () => console.log("button pressed - Yes")}, {text: "No", onPress: () => console.log("button pressed - No")}] )}/>
+// const containerStyle = { backgroundColor: "purple"}
+
+// for prompt: Alert.prompt("my title", "my message", text => console.log(text))} />
