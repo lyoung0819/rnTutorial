18d84fcba558c35b012fcb4f978a4eb686adc48a
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-10-26 14:51:53 -0500

    initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hey Zack - this is a mobile app. Building Buckets!!! EEEP</Text>
       <StatusBar style="auto" />
     </View>
   );
